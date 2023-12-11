203565ea5abb1260b65342187036c8c9576f895b
Asad Memon <asad.lionpk@gmail.com>
2023-12-10 16:16:45 -0800

    lobby fixes

--- examples/r3f-readyplayerme/src/Lobby.jsx
+++ examples/r3f-readyplayerme/src/Lobby.jsx
@@ -1,10 +1,12 @@
-import imgPlayroom from "./assets/logo-playroom.png";
-import imgReadyPlayerMe from "./assets/logo-rpm.png";
+import imgLogo from "./assets/logo-rr.svg";
+import imgReadyPlayerMe from "./assets/bottom-logo.svg";
 
 function Lobby({onJoinOrCreateRoom}) {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <div className="text-4xl font-bold">Ready<br/>Rooms</div>
+      <div className="text-4xl font-bold">
+        <img src={imgLogo} alt="Ready Rooms" style={{height: '80px'}}/>
+      </div>
       <div className="text-md mt-1">OPEN HANGOUT SPACES</div>
       <div className="flex mt-20">
         <button className="rounded-lg px-3 py-2 m-3" style={{backgroundColor: "#A2DCBB"}} onClick={()=> onJoinOrCreateRoom()}>New room</button>
@@ -15,11 +17,8 @@ function Lobby({onJoinOrCreateRoom}) {
           }
         }}>Join room</button>
       </div>
-      <div className="absolute bottom-3 text-xs gap-2 flex items-center">
-        
-          <img src={imgReadyPlayerMe} alt="Ready Player Me" style={{height: '40px'}}/>
-           <b>x</b>
-          <img src={imgPlayroom} alt="Playroom" style={{height: '20px'}}/>
+      <div className="absolute bottom-5 text-xs gap-2 flex items-center">
+          <img src={imgReadyPlayerMe} alt="Ready Player Me x Playroom" style={{height: '40px'}}/>
       </div>
     </div>
   );
